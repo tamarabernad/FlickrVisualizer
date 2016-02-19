18f5af8aca7c208211d71dc5fe1d5b24815089f8
tamarabernad <tamara@codedazur.es>
2016-02-19 18:48:06 +0100

    Initial Commit

---
+++ FlickrVisualizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrVisualizer
+//
+//  Created by Tamara Bernad on 19/02/16.
+//  Copyright © 2016 Tamara Bernad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrVisualizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrVisualizer
+//
+//  Created by Tamara Bernad on 19/02/16.
+//  Copyright © 2016 Tamara Bernad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
